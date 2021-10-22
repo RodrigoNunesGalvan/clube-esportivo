bf067e392ebb0c9e6d1e371903cbe844d9e3a6f4
Rodrigo Nunes <rodrigongalvan@hotmail.com>
2021-10-21 21:48:30 -0300

    Criando Micro Serviço de cadastro de usuario para clube esportivo

---
+++ src/test/java/br/com/clube/esportivo/EsportivoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.clube.esportivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsportivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
